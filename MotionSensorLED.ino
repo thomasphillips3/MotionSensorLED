813afc5812c67c7dea051e9ad5c0ca1dc98a6b31
Thomas Phillips <thomasphillips3@gmail.com>
2016-10-29 15:04:35 -0700

    Turn on 2 LEDs when motion is detected.

--- MotionSensorLED.ino
+++ MotionSensorLED.ino
@@ -1,9 +1,27 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
 
+int MOTION_PIN = 7;
+int LED_1 = 6;
+int LED_2 = 5;
+
+void setup() {
+  pinMode(MOTION_PIN, INPUT);
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  long sMotion = digitalRead(MOTION_PIN);
+  if (sMotion == HIGH) {
+    digitalWrite(LED_1, HIGH);
+    digitalWrite(LED_2, HIGH);
+    delay(1000);
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, LOW);
+  } else {
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, LOW);
+  }
 
 }
